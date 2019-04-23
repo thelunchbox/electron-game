ef7035419ce17bbe39adf5e1141ff0f8e21dd4d9
Alex Merkert <alex.merkert@gmail.com>
2019-04-22 23:18:24 -0400

    adding player

--- script/states/game.js
+++ script/states/game.js
@@ -3,11 +3,21 @@ const { STATES } = require('../stateFactory');
 
 class Game extends State {
 
+    constructor(args) {
+        super(args);
+
+        this.player = {
+            x: 800,
+            y: 800,
+        };
+    }
+
     update(dt, keys) {
         console.log(keys);
     }
 
     draw(renderer) {
+
     }
 }
 
