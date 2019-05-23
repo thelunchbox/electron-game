2b91fa9481f8056fc080b48ccb8aabee7f6bed00
Brandon Burkhardt <brandon.burkhardt@crosschx.com>
2019-05-23 09:47:27 -0400

    added player class update function

--- script/gameObjects/player.js
+++ script/gameObjects/player.js
@@ -1,7 +1,8 @@
-const { INSECT_CONSTANTS, PLAYER_CONSTANTS } = require('../constants');
+const { GAME_CONSTANTS, INSECT_CONSTANTS, PLAYER_CONSTANTS } = require('../constants');
 
+const { GAME_HEIGHT, GAME_WIDTH } = GAME_CONSTANTS;
 const { INSECT_SIZE, INSECT_SPEED } = INSECT_CONSTANTS;
-const { RIBBIT_REST, TOUNGUE_TIP_SIZE } = PLAYER_CONSTANTS;
+const { FROG_SIZE, MAX_INJURY, RIBBIT_REST, SPEED, TOUNGUE_TIP_SIZE } = PLAYER_CONSTANTS;
 
 class Player {
     constructor(x, y) {
@@ -21,13 +22,93 @@ class Player {
         };
     }
 
-    update(player) {
-        if (player.ribbit.cooldown > 0) {
-            player.ribbit.cooldown -= 1;
-            if (player.ribbit.cooldown == 0) {
-                player.ribbit.x = null;
-                player.ribbit.y = null;
+    update(keys, frame, flies, bees) {
+        this._recoverInjury();
+        this._handleTongue(keys, frame, flies, bees);
+        this._handleRibbit(keys);
+        this._movePlayer(keys);
+    }
+
+    _handleTongue(keys, frame, flies, bees) {
+        if (this.tongue.active) {
+            const tFrame = frame - this.tongue.frame;
+            const tDiff = ((tFrame - 15) ** 2) / 5;
+            if (this.tongue.active === 'extend') {
+                this.tongue.length = this.tongue.length + tDiff;
+                if (tFrame == 15) {
+                    this.tongue.active = 'retract';
+                }
             }
+
+            if (this.tongue.active === 'retract') {
+                this.tongue.length = Math.max(0, this.tongue.length - tDiff);
+                if (this.tongue.length == 0) {
+                    this.tongue.active = null;
+                    flies.filter(fly => fly.trapped).forEach(fly => {
+                        const index = flies.indexOf(fly);
+                        flies.splice(index, 1);
+                        this.score += 0.5;
+                    });
+                    bees.filter(bee => bee.trapped).forEach(bee => {
+                        const index = bees.indexOf(bee);
+                        bees.splice(index, 1);
+                        this.score -= 5;
+                        this.injury = MAX_INJURY;
+                    });
+                }
+            }
+        } else {
+            if (keys.includes(70) && !this.injury) {
+                this.tongue.active = 'extend';
+                this.tongue.frame = frame;
+            }
+        }
+    }
+
+    _recoverInjury() {
+        if (this.injury) {
+            this.injury -= 1;
+        }
+    }
+
+    _handleRibbit(keys) {
+        if (this.ribbit.cooldown > 0) {
+            this.ribbit.cooldown -= 1;
+            if (this.ribbit.cooldown == 0) {
+                this.ribbit.x = null;
+                this.ribbit.y = null;
+            }
+        }
+
+        if (!this.tongue.active) {
+            if (keys.includes(32) && !this.ribbit.cooldown) {
+                this.ribbit = {
+                    cooldown: RIBBIT_REST,
+                    x: this.x,
+                    y: this.y,
+                };
+            }
+        }
+    }
+
+    _movePlayer(keys) {
+        if (!this.tongue.active) {
+            if (keys.includes(38)) {
+                this.y -= SPEED;
+            } else if (keys.includes(40)) {
+                this.y += SPEED;
+            }
+    
+            if (keys.includes(37)) {
+                this.x -= SPEED;
+                this.dir = -1;
+            } else if (keys.includes(39)) {
+                this.x += SPEED;
+                this.dir = 1;
+            }
+
+            this.x = Math.min(GAME_WIDTH - FROG_SIZE / 2, Math.max(FROG_SIZE / 2, this.x));
+            this.y = Math.min(GAME_HEIGHT - FROG_SIZE / 2, Math.max(FROG_SIZE / 2, this.y));
         }
     }
 }
