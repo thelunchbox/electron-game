b392d104940d9fc87bb2bb23f7e06c1ca9609b43
Brandon Burkhardt <brandon.burkhardt@crosschx.com>
2019-05-23 09:47:14 -0400

    removed unnecessary passing of self to class method (insect => this)

--- script/gameObjects/insect.js
+++ script/gameObjects/insect.js
@@ -14,38 +14,42 @@ class Insect {
         this.trapped = false;
     }
 
-    update(insect, player) {
+    update(player) {
         const { x, y, dir, tongue } = player;
         if (tongue.active) {
             const tongueX = x + dir * (tongue.length * Math.cos(-Math.PI / 4));
             const tongueY = y + (tongue.length * Math.sin(-Math.PI / 4));
-            const diffX = Math.abs(insect.x - tongueX);
-            const diffY = Math.abs(insect.y - tongueY);
+            const diffX = Math.abs(this.x - tongueX);
+            const diffY = Math.abs(this.y - tongueY);
             // did we eat an insect?
             if (diffX <= TONGUE_TIP_SIZE + INSECT_SIZE + 2 * INSECT_SPEED && diffY <= TONGUE_TIP_SIZE + INSECT_SIZE + 2 * INSECT_SPEED) {
                 // yup
-                insect.trapped = true;
+                this.trapped = true;
             }
         }
 
-        if (insect.trapped) {
-            insect.x = x + dir * (tongue.length * Math.cos(-Math.PI / 4)) + dir * 33;
-            insect.y = y + (tongue.length * Math.sin(-Math.PI / 4)) - 23;
-        } else if (insect.target) {
-            const diffX = Math.abs(insect.x - insect.target.x);
-            const diffY = Math.abs(insect.y - insect.target.y);
+        if (this.trapped) {
+            this.x = x + dir * (tongue.length * Math.cos(-Math.PI / 4)) + dir * 33;
+            this.y = y + (tongue.length * Math.sin(-Math.PI / 4)) - 23;
+        } else if (this.target) {
+            const diffX = Math.abs(this.x - this.target.x);
+            const diffY = Math.abs(this.y - this.target.y);
             if (diffX <= INSECT_SPEED && diffY <= INSECT_SPEED) {
-                insect.target = {
+                this.target = {
                     x: Math.random() * GAME_WIDTH,
                     y: Math.random() * GAME_HEIGHT,
                 };
             }
 
-            const angle = Math.atan2(insect.target.y - insect.y, insect.target.x - insect.x);
-            insect.x += INSECT_SPEED * Math.cos(angle);
-            insect.y += INSECT_SPEED * Math.sin(angle);
+            const angle = Math.atan2(this.target.y - this.y, this.target.x - this.x);
+            this.x += INSECT_SPEED * Math.cos(angle);
+            this.y += INSECT_SPEED * Math.sin(angle);
         }
-    }
+    };
+
+    draw(renderer) {
+
+    };
 }
 
 module.exports = Insect;
